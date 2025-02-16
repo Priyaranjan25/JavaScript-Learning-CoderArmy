00fec002ad62580cf46c145d461f9eff97f34281
Priyaranjan <info.prj4916@gmail.com>
2025-02-16 20:02:40 +0530

    Day01 first commit

---
+++ Day01/first.js
@@ -0,0 +1,14 @@
+console.log("Hello Coder Army")
+
+// let const var
+// var is old we use let insteed of var.
+
+let num=10;
+console.log(num);
+
+let name= "Priyo";
+console.log(name);
+
+const ids =20;
+console.log(ids);
+// in const values can't be changed.
