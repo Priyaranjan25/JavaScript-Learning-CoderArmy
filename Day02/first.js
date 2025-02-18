67897fc2189ff08526f33fb033ca9550aaee78a3
Priyaranjan <info.prj4916@gmail.com>
2025-02-18 21:27:12 +0530

    Day02 first commit

---
+++ Day02/first.js
@@ -0,0 +1,36 @@
+// declare the variable
+let num=10;
+console.log(num);
+
+//number
+let account_balance =20;
+console.log(account_balance);
+console.log( typeof account_balance);
+
+//string
+let str ="Priyaranjan";
+console.log(str);
+console.log( typeof str);
+
+//Boolean true or false
+let statement =true
+console.log(statement);
+console.log(typeof statement);
+
+//Undefined
+let account
+console.log(account)
+
+//Null
+let balance = null
+console.log(balance)
+
+//bigInt
+let Bnum =154549665498494991454n;
+console.log(Bnum)
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+
+
+
